6c0383823771744f862e692492d91cfb249548a3
Diegod96 <ddphillyfan@gmail.com>
2020-04-19 19:53:56 -0400

    Update

--- run.py
+++ run.py
@@ -7,6 +7,8 @@ import time
 
 if __name__ == '__main__':
 
+    # Runs main() every 5 hours
+    # You can change the amount of time and time interval by tweaking the "schedule.ever()..."
     main()
     schedule.every(5).hours.do(main)
     while True:
