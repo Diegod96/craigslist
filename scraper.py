549a20fafd9507985dd91786934a187a79b2e104
Diegod96 <ddphillyfan@gmail.com>
2020-04-19 16:58:17 -0400

    version 1

--- scraper.py
+++ scraper.py
@@ -12,7 +12,14 @@ from url import *
 class Job:
 
     def __init__(self):
-        self.driver = webdriver.Firefox("geckodriver.exe")
+
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        self.driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        # self.driver = webdriver.Firefox()
         self.delay = 10  # The delay the driver gives when loading the web page
 
     # Load up the web page
